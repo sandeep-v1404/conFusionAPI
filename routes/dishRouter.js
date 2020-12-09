de8cd3634183fee57b9dd50a1dfc0fafa44f9ee8
unknown <valpasanisandeep@gmail.com>
2020-12-10 03:23:34 +0530

    Mongoose Population

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter
     .route("/")
     .get((req, res, next) => {
         Dishes.find({})
+            .populate("comments.author")
             .then(
                 (dishes) => {
                     res.statusCode = 200;
@@ -56,6 +57,7 @@ dishRouter
     .route("/:dishId")
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+            .populate("comments.author")
             .then(
                 (dish) => {
                     res.statusCode = 200;
@@ -102,6 +104,7 @@ dishRouter
     .route("/:dishId/comments")
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+            .populate("comments.author")
             .then(
                 (dish) => {
                     if (dish != null) {
@@ -123,12 +126,17 @@ dishRouter
             .then(
                 (dish) => {
                     if (dish != null) {
+                        req.body.author = req.user._id;
                         dish.comments.push(req.body);
                         dish.save().then(
                             (dish) => {
-                                res.statusCode = 200;
-                                res.setHeader("Content-Type", "application/json");
-                                res.json(dish);
+                                Dishes.findById(dish._id)
+                                    .populate("comments.author")
+                                    .then((dish) => {
+                                        res.statusCode = 200;
+                                        res.setHeader("Content-Type", "application/json");
+                                        res.json(dish);
+                                    });
                             },
                             (err) => next(err)
                         );
@@ -181,6 +189,7 @@ dishRouter
     .route("/:dishId/comments/:commentId")
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+            .populate("comments.author")
             .then(
                 (dish) => {
                     if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -223,9 +232,13 @@ dishRouter
                         }
                         dish.save().then(
                             (dish) => {
-                                res.statusCode = 200;
-                                res.setHeader("Content-Type", "application/json");
-                                res.json(dish);
+                                Dishes.findById(dish._id)
+                                    .populate("comments.author")
+                                    .then((dish) => {
+                                        res.statusCode = 200;
+                                        res.setHeader("Content-Type", "application/json");
+                                        res.json(dish);
+                                    });
                             },
                             (err) => next(err)
                         );
@@ -251,9 +264,13 @@ dishRouter
                         dish.comments.id(req.params.commentId).remove();
                         dish.save().then(
                             (dish) => {
-                                res.statusCode = 200;
-                                res.setHeader("Content-Type", "application/json");
-                                res.json(dish);
+                                Dishes.findById(dish._id)
+                                    .populate("comments.author")
+                                    .then((dish) => {
+                                        res.statusCode = 200;
+                                        res.setHeader("Content-Type", "application/json");
+                                        res.json(dish);
+                                    });
                             },
                             (err) => next(err)
                         );
