50f106106a012f068073dcd1a6c9dab113b4cadf
sandeep-v1404 <valpasanisandeep@gmail.com>
2020-12-09 22:41:05 +0530

    Passport

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var leaderRouter = require("./routes/leaderRouter");
 const Dishes = require("./models/dishes");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var app = express();
 const url = "mongodb://localhost:27017/conFusion";
@@ -46,24 +48,19 @@ app.use(
         store: new FileStore(),
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-    console.log(req.session);
-
-    if (!req.session.user) {
+    if (!req.user) {
         var err = new Error("You are not authenticated!");
         err.status = 403;
         return next(err);
     } else {
-        if (req.session.user === "authenticated") {
-            next();
-        } else {
-            var err = new Error("You are not authenticated!");
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
