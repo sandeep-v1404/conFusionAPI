838dc48cad95d00fbc85ffb20b7dfa0498bdee9e
unknown <valpasanisandeep@gmail.com>
2020-12-09 21:52:08 +0530

    Express Session Part 2 Fix

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
 connect.then(
-  (db) => {
-    console.log("Connected correctly to server");
-  },
-  (err) => {
-    console.log(err);
-  }
+    (db) => {
+        console.log("Connected correctly to server");
+    },
+    (err) => {
+        console.log(err);
+    }
 );
 
 // view engine setup
@@ -38,55 +38,55 @@ app.use(cookieParser());
 
 //app.use(cookieParser("12345-67890-09876-54321"));
 app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
+    session({
+        name: "session-id",
+        secret: "12345-67890-09876-54321",
+        saveUninitialized: false,
+        resave: false,
+        store: new FileStore(),
+    })
 );
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+    console.log(req.session);
 
-  if (!req.session.user) {
-    var err = new Error("You are not authenticated!");
-    err.status = 403;
-    return next(err);
-  } else {
-    if (req.session.user === "authenticated") {
-      next();
+    if (!req.session.user) {
+        var err = new Error("You are not authenticated!");
+        err.status = 403;
+        return next(err);
     } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
+        if (req.session.user === "authenticated") {
+            next();
+        } else {
+            var err = new Error("You are not authenticated!");
+            err.status = 403;
+            return next(err);
+        }
     }
-  }
 }
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
